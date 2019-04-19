59da3857df6d3b3afa29ec86c67b56a5b0314bd6
Evgeny Im <evgenyim6456@gmail.com>
2019-04-19 20:13:08 +0300

    Change called method names in test_folder.py

--- task06/test_folder.py
+++ task06/test_folder.py
@@ -6,38 +6,38 @@ from printer import pretty_print
 
 def test_num_num():
     const_folder = ConstantFolder()
-    actual = const_folder.fold(BinaryOperation(Number(19), '-', Number(7)))
+    actual = const_folder.visit(BinaryOperation(Number(19), '-', Number(7)))
     assert actual == Number(12)
-    actual = const_folder.fold(BinaryOperation(Number(11), '*', Number(9)))
+    actual = const_folder.visit(BinaryOperation(Number(11), '*', Number(9)))
     assert actual == Number(99)
 
 
 def test_num_ref():
     const_folder = ConstantFolder()
-    actual = const_folder.fold(BinaryOperation(Number(0),
-                                               '*', Reference('tmp')))
+    actual = const_folder.visit(BinaryOperation(Number(0),
+                                                '*', Reference('tmp')))
     assert actual == Number(0)
 
 
 def test_ref_num():
     const_folder = ConstantFolder()
-    actual = const_folder.fold(BinaryOperation(Reference('tmp'),
-                                               '*', Number(0)))
+    actual = const_folder.visit(BinaryOperation(Reference('tmp'),
+                                                '*', Number(0)))
     assert actual == Number(0)
 
 
 def test_ref_ref():
     const_folder = ConstantFolder()
-    actual = const_folder.fold(BinaryOperation(Reference('tmp'),
-                                               '-', Reference('tmp')))
+    actual = const_folder.visit(BinaryOperation(Reference('tmp'),
+                                                '-', Reference('tmp')))
     assert actual == Number(0)
 
 
 def test_un_num():
     const_folder = ConstantFolder()
-    actual = const_folder.fold(UnaryOperation('-', Number(10)))
+    actual = const_folder.visit(UnaryOperation('-', Number(10)))
     assert actual == Number(-10)
-    actual = const_folder.fold(UnaryOperation('!', Number(10)))
+    actual = const_folder.visit(UnaryOperation('!', Number(10)))
     assert actual == Number(0)
 
 
