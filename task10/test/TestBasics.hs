995085bab8682ecef93c7c8e07f5a47f1d936177
Evgeny Im <evgenyim6456@gmail.com>
2019-05-11 01:49:58 +0300

    Fix first part

--- task10/test/TestBasics.hs
+++ task10/test/TestBasics.hs
@@ -17,6 +17,9 @@ testsBasics = testGroup "Unit tests for Basics tasks"
     , testCase "tail' works on non-empty list too" $
         tail' [1,2,3] @?= [2,3]
 
+    , testCase "tail' works on non-empty infinite list too" $
+        take' 10 (tail' [1..]) @?= take 10 [2..]
+
     , testCase "take' takes 1 element from 3-element list" $
         take' 1 [1,2,3] @?= [1]
 
@@ -27,10 +30,13 @@ testsBasics = testGroup "Unit tests for Basics tasks"
         drop' 1 [1,2,3] @?= [2,3]
 
     , testCase "drop' drops 6 element from infinity list" $
-            head'(drop' 6 [1..]) @?= 7
+        take' 10 (drop' 6 [1..]) @?= take 10 [7..]
 
     , testCase "filter' selects only even numbers from 0 to 10" $
         filter' even [0..10] @?= [0,2..10]
+
+    , testCase "filter' selects only even numbers from all positive numbers" $
+        take' 10 (filter' even [1..]) @?= take 10 ([2,4..])
 
     , testCase "foldl'' can be used for finding sum of elements" $
         foldl'' (+) 0 [1,2,3] @?= 6
@@ -38,8 +44,11 @@ testsBasics = testGroup "Unit tests for Basics tasks"
     , testCase "concat' works on finite lists as expected" $
         concat' [1,2,3] [4,5,6] @?= [1..6]
 
-    , testCase "concat' works on infinite lists as expected" $
-            take' 6 (concat' [1,2,3] [1..]) @?= [1,2,3,1,2,3]
+    , testCase "concat' works when first list is infinite as expected" $
+        take' 6 (concat' [1..] [1,2,3]) @?= take' 6 ([1..])
+
+    , testCase "concat' works when second list is infinite as expected" $
+        take' 6 (concat' [1,2,3] [1..]) @?= [1,2,3,1,2,3]
 
     , testCase "quickSort actualy sorts the list" $
         quickSort' [5,2,3,4,1] @?= [1..5]
